47203645705099e3005b0008bd27634f914dacf4
palatej <palatejean@gmail.com>
2025-03-25 18:24:04 +0100

    Time varying arima  + upgrade to 2.3.0.

---
+++ jdplus-incubator-base/jdplus-advancedsa-base-parent/jdplus-advancedsa-base-core/src/test/java/jdplus/advancedsa/base/core/tarima/LtdAirlineItemTest.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2025 JDemetra+.
+ * Licensed under the EUPL, Version 1.2 or – as soon they will be approved
+ * by the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ *      https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ */
+package jdplus.advancedsa.base.core.tarima;
+
+import tck.demetra.data.Data;
+
+/**
+ *
+ * @author Jean Palate
+ */
+public class LtdAirlineItemTest {
+
+//    double[] s = DoubleSeq.of(Data.RETAIL_ALLHOME).log().toArray();
+
+    public LtdAirlineItemTest() {
+    }
+
+}
